638958c63d4f582e69269a4fc7f7af329df694a9
Ken Marchand <marchand_kenneth@smc.edu>
2020-10-03 19:36:45 -0700

    Added inquirer

---
+++ app.js
@@ -0,0 +1,43 @@
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
+
+// //const profileDataArgs = process.argv.slice(2);
+
+// const [name, github] = profileDataArgs;
+
+
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// // Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
+  
+//     console.log('================');
+  
+//     // Is the same as this...
+    
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+//   };
+  
+  
+//   printProfileData(profileDataArgs);
